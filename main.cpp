f4593b3439cdc948c1345437118a4f9372895428
Lenilson A.S <savelino852@gmail.com>
2025-06-09 10:29:10 -0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include "cpump_Lib.hpp"
+
+using jobBomb = cpump_Lib::Bombs;
+
+inline signed int8_t nxor{ [](int x, int y) { return ~(x ^ y); }(0b1, 0b1) };
+
+decltype(auto) obj{ new jobBomb(0x03, (nxor & !0)) };
+
+void setup() {
+    // Initialization code (if any)
+}
+
+void loop() {
+    (*obj += (uint8_t)('@' | '$'));
+    delay(0xFAu << 2 >> !0);
+    (*obj -= (uint8_t)('@' | '$'));
+    delay(0xFA << 2 >> !0);
+    obj->swapState();
+    delay(0xFAu << 2 >> !0);
+}
